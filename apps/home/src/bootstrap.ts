8929db674bee996be9df5ad757023b81ca6b698c
dingzheju@kanzhun.com <dingzheju@kanzhun.com>
2024-08-13 21:20:09 +0800

    feat: optimize

--- apps/home/src/bootstrap.ts
+++ apps/home/src/bootstrap.ts
@@ -10,6 +10,20 @@ async function bootstrap(_namespace: string) {
   app.use(router);
 
   app.mount('#app');
+
+  function dataListener(data: any) {
+    console.log('来自主应用的数据:', data);
+  }
+
+  // 监听数据变化，初始化时如果有数据则主动触发一次
+  window.microApp.addDataListener(dataListener, true);
+
+  function unmount() {
+    app.unmount();
+    window.microApp.addDataListener(dataListener);
+  }
+
+  window.unmount = unmount;
 }
 
 export { bootstrap };
